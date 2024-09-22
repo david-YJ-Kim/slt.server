97d4de9307761cd008cf1cdb22e54b755e294962
DavidKim <115195328+david-YJ-Kim@users.noreply.github.com>
2024-09-22 15:45:33 -0400

    Provision 관련 대응 인터페이스 개발

--- src/main/java/com/tsh/slt/interfaces/rest/ProductProvisionController.java
+++ src/main/java/com/tsh/slt/interfaces/rest/ProductProvisionController.java
@@ -1,30 +1,55 @@
 package com.tsh.slt.interfaces.rest;
 
 
+import com.tsh.slt.interfaces.util.ApMessageList;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/util")
+@RequestMapping("/provision")
 @RequiredArgsConstructor
 @Slf4j
 public class ProductProvisionController {
 
-//    @Autowired
-//    WfsAlarmReportImpl wfsAlarmReport;
-//
-//    @PostMapping(WorkManMessageList.WFS_ALARM_REPORT)
-//    public ApFlowProcessVo executeWfsAlarmReport(@RequestBody WfsAlarmReportIvo wfsAlarmReportIvo,
-//                                                 @RequestParam(value = "key") String trackingKey,
-//                                                 @RequestParam(value = "scenario") String scenarioType) throws Exception {
-//
-//
-//        String cid = WorkManMessageList.WFS_ALARM_REPORT;
-//        ApFlowProcessVo apFlowProcessVo = this.wfsAlarmReport.initialize(cid, trackingKey, scenarioType);
-//
-//        return this.wfsAlarmReport.execute(apFlowProcessVo, wfsAlarmReportIvo);
-//
-//    }
+
+    @GetMapping(ApMessageList.PROV_INSTALL_VER_REQ)
+    public ResponseEntity<String> execute(){
+        // TODO DB 조회하여 버전정보 리턴, 현재는 하드코딩
+        return new ResponseEntity<>("1.0.0", HttpStatus.OK);
+    }
+
+    @GetMapping(ApMessageList.PROV_INSTALL_FILE_REQ)
+    public ResponseEntity<byte[]> execute(@RequestParam(value = "type") String type) throws Exception {
+
+        try{
+            // TODO 테스트 대응, 프로젝트 내부 경로 사용
+            Path path = Paths.get(new ClassPathResource("static/stl.server-0.0.1-SNAPSHOT.jar").getURI());
+            byte[] fileBytes = Files.readAllBytes(path);
+
+            // 응답 헤더 설정
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);  // 바이너리 데이터
+            headers.setContentDispositionFormData("attachment", "service.jar"); // 다운로드 되는 파일 이름 설정
+
+            return new ResponseEntity<>(fileBytes, headers, HttpStatus.OK);
+
+        }catch (IOException ioErr){
+
+            ioErr.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
 }
--- src/main/java/com/tsh/slt/interfaces/util/ApMessageList.java
+++ src/main/java/com/tsh/slt/interfaces/util/ApMessageList.java
@@ -3,4 +3,10 @@ package com.tsh.slt.interfaces.util;
 public final class ApMessageList {
 
     public static final String AP_HEALTH_TEST = "AP_HEALTH_TEST";
+
+    /* 서비스 Install 의 버전 정보 */
+    public static final String PROV_INSTALL_VER_REQ = "PROV_INSTALL_VER_REQ";
+
+    /*Install 필요한 파일들을 Return */
+    public static final String PROV_INSTALL_FILE_REQ = "PROV_INSTALL_FILE_REQ";
 }
